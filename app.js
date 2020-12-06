537021d79c90be1e2d62de3ae9881e2741c793a0
Jaynil1611 <48921037+Jaynil1611@users.noreply.github.com>
2020-12-07 00:09:35 +0530

    feat: added the index, style and css pages

---
+++ app.js
@@ -0,0 +1,47 @@
+
+var textInput = document.querySelector("#text-input");
+
+var outputDiv = document.querySelector("#output-div");
+
+var translateButton = document.querySelector("#trans-button");
+
+var clearButton = document.querySelector("#clear-button");
+
+var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var url = 'https://api.funtranslations.com/translate/ebonics.json';
+
+
+function executeFetchCall(inputText) {
+    fetch(getTranslatedURL(inputText))
+        .then(response => response.json())
+        .then(resp => {
+            // console.log(resp);
+            outputDiv.innerText = resp.contents.translated;
+        })
+        .catch(errorHandler);
+}
+
+function getTranslatedURL(inputText) {
+    var encodedURI = encodeURI(inputText);
+    return `${url}?text=${encodedURI}`;
+}
+
+function errorHandler(error) {
+    alert("The server is busy. Please try again after some time.");
+    console.log(error);
+}
+
+function clickHandler(event) {
+    if (textInput.value !== "") {
+        executeFetchCall(textInput.value)
+    }
+    else alert("Please enter text");
+}
+
+function clearClickHandler(event) {
+    textInput.value = "";
+    outputDiv.innerHTML = "";
+}
+
+clearButton.addEventListener('click',clearClickHandler)
+translateButton.addEventListener('click', clickHandler);
